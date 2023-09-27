ce2a7f459a03327d1085db2bb7e56f33f372cc29
Pieter Edwards <PieterEd12@Gmail.com>
2023-09-27 20:02:18 +0000

    Retrieves sign in and sign out button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect, signOut, getRedirectResult } from 'firebase/auth';
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyDndU1jJYP9rb87J0UixYbpzn6MpvXOLms",
@@ -18,12 +18,38 @@ document.getElementById("googleSignIn").addEventListener("click", () => {
     signInWithRedirect(auth, provider);
 });
 
+
+window.onload = getRedirectResult(auth)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        const email = error.customData.email;
+
+        const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+
+document.getElementById("googleSignOut").addEventListener("click", () => {
+    signOut(auth);
+});
+
 // Detect Auth State
 
 onAuthStateChanged(auth, user => {
     if (user != null) {
         console.log(user);
+        document.getElementById("googleSignOut").hidden = false;
+        document.getElementById("googleSignIn").hidden = true;
     } else {
         console.log("No user");
+        document.getElementById("googleSignOut").hidden = true;
+        document.getElementById("googleSignIn").hidden = false;
     }
 });
