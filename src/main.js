d7986d71550ad2102806c8035b26341d482e3a6f
Pieter Edwards <PieterEd12@Gmail.com>
2023-11-16 19:43:57 +0000

    Removed add button, added zoom controls

--- src/main.js
+++
@@ -1,9 +0,0 @@
-function showAddMenu() {
-    const bg = document.getElementsByClassName("ABbackground");
-
-    if (bg != '20vh') {
-        bg.style.height = '20vh';
-    } else {
-        bg.style.height = '0';
-    }
-}
