2a02e0fb51435474c8fb466323075a6b0b2d1aa6
Pieter <pieterkurtedwards@gmail.com>
2024-01-14 00:12:19 +0000

    Positioning for settings elements

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 function openSettings() {
     document.getElementById("settingsContainer").classList.remove("closeSettings");
     document.getElementById("settingsContainer").classList.add("openSettings");
+
+    setTimeout(function() {
+        document.getElementById("settingsContent").style.display = 'flex';
+    }, 1500)
 }
 
 function closeSettings() {
     document.getElementById("settingsContainer").classList.add("closeSettings");
     document.getElementById("settingsContainer").classList.remove("openSettings");
+
+    document.getElementById("settingsContent").style.display = 'none';
 }
 
 export { openSettings, closeSettings};
