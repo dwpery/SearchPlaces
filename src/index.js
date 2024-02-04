d010fcecf546612a0c084cb791047dc96c9fffdf
Pieter <pieterkurtedwards@gmail.com>
2024-02-04 20:57:22 +0000

    Sign in / up with Email and Password code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyDndU1jJYP9rb87J0UixYbpzn6MpvXOLms",
@@ -19,9 +19,45 @@ const auth = getAuth(firebaseApp);
 // Detects auth state change
 
 onAuthStateChanged(auth, user => {
+    document.getElementById('loader').hidden = true;
+    document.getElementById('app').hidden = false;
     if (user != null) {
         console.log("Logged In!");
+        document.getElementById('signedOut').hidden = true;
+        document.getElementById('signedIn').hidden = false;
     } else {
-        console.log("Logged out!")
+        console.log("Logged out!");
+        document.getElementById('signedOut').hidden = false;
+        document.getElementById('signedIn').hidden = true;
     }
+});
+
+document.getElementById("signUp").addEventListener('click', () => {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode + " " + errorMessage);
+        });
+});
+
+document.getElementById("logIn").addEventListener('click', () => {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => { 
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode + " " + errorMessage);
+        });
 });
