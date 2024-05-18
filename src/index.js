0f2dabfaf637023140c0e6f07e8ff4d544572895
Pieter <pieterkurtedwards@gmail.com>
2024-05-18 23:18:39 +0100

    Hides editor UI

--- src/index.js
+++ src/index.js
@@ -148,23 +148,34 @@ document.getElementById('zoomOut').addEventListener('click', () => {
 });
 
 // Toolbox opening
-
 document.getElementById('toolboxBtn').addEventListener('click', () => {
     document.getElementById("toolbox").classList.add("openSideMenu");
     document.getElementById("properties").classList.remove("openSideMenu");
 })
 
+// Toolbox closing
 document.getElementById('closeToolbox').addEventListener('click', () => {
     document.getElementById("toolbox").classList.remove("openSideMenu");
 })
 
+// Properties opening
 document.getElementById('propertiesBtn').addEventListener('click', () => {
     document.getElementById("properties").classList.add("openSideMenu");
     document.getElementById("toolbox").classList.remove("openSideMenu");
 })
 
+// Properties closing
 document.getElementById('closeProperties').addEventListener('click', () => {
     document.getElementById("properties").classList.remove("openSideMenu");
+})
+
+// Hide Editor UI
+document.getElementById('hideUiBtn').addEventListener('click', () => {
+    document.getElementById('leftSideTools').classList.add('lstHide');
+
+    setTimeout(() => {
+        document.getElementById('leftSideTools').style.display = 'none';
+    }, 500);
 })
 
 // Properties buttons
